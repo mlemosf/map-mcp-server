6c6f94b54ab8417ad251a037e95e5956213b9124
matheus <matheus@notedebian.notedebian>
2025-08-24 15:35:36 -0300

    feat: Lista de features

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/{layer}/features")
+def get_layer_features(layer):
+    gdf = gpd.read_file("~/Downloads/classes_de_solos.json")
+    features = list(gdf.columns)
+    return {
+        'layer': layer,
+        'feature_list': features
+    }
+    
+
 @app.get("/{layer}/area/{feature}")
 def get_features_area(layer, feature):
-        gdf = gpd.read_file("/tmp/classes_de_solos.json")
+        gdf = gpd.read_file("~/Downloads/classes_de_solos.json")
         feature_area = gdf.groupby(feature).area.sum()
         print(feature_area)
         feature_list = [{'feature_class': i.lower(), 'total_area': j} for i,j in feature_area.items()]
@@ -23,7 +33,7 @@ def get_features_area(layer, feature):
 
 @app.get("/{layer}/area/{feature}/{feature_class}")
 def get_feature_area(layer, feature, feature_class):
-        gdf = gpd.read_file("/tmp/classes_de_solos.json")
+        gdf = gpd.read_file("~/Downloads/classes_de_solos.json")
         feature_data = gdf[gdf[feature] == feature_class]
 
         feature_area = feature_data.area.sum()
